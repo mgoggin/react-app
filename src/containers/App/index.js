9c32db9f35823718d297b282f3e2d17bcd54aed9
Mike Goggin <michael@mgoggin.com>
2018-03-04 10:09:03 -0600

    Wrap component in `<App />` container

---
+++ src/containers/App/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import Hi from 'components/Hi';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Hi />
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hi from 'components/Hi';
+import App from 'containers/App';
 import 'index.scss';
 
-ReactDOM.render(<Hi />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
